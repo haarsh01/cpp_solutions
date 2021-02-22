d939095b957dfe7536e2c8e3cc93088804368506
Harsh Kaushik <60865477+haarsh01@users.noreply.github.com>
2021-02-22 11:39:20 +0530

    Add files via upload

---
+++ searching and sorting/checkarrayrotation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int arrayRotateCheck(int *input, int size){
+	for(int i=0; i<size-1; i++){
+		if(input[i] > input[i+1]){
+			return i+1;
+		}
+
+	}
+	return 0;
+}
+
+int main(){
+	int n;
+	cin >> n;
+	int input[100];
+	for(int i=0; i <n; i++){
+		cin >> input[i];
+	}
+	cout << arrayRotateCheck(input, n) << endl;
+	}
